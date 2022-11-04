f833afeb5b3a5b7ddd341d77f643834db5c9b396
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2022-11-04 19:58:20 -0400

    fixed constants file being added as reference

---
+++ UseCasesCoreData/Constants.swift
@@ -0,0 +1,35 @@
+//
+//  Constants.swift
+//  UseCasesCoreData
+//
+//  Created by Ethan Kisiel on 11/4/22.
+//
+
+import Foundation
+import Neumorphic
+import SwiftUI
+
+let EMPTY_STRING: String = ""
+let USER_ID_STRING: String = "UserId"
+
+let TRASH_ICON: String = "trash.circle.fill"
+let UNCHECKED_ICON: String = "square"
+let CHECKED_ICON: String = "square.inset.filled"
+let MORE_ICON: String = "ellipsis.circle"
+let LESS_ICON: String = "minus.circle"
+
+// neumorphic UI main color
+let NM_MAIN = Color.Neumorphic.main
+let NM_LIGHT_SHADOW = Color.Neumorphic.lightShadow
+// neumorphic UI secondary color
+let NM_SEC = Color.Neumorphic.secondary
+let NM_DARK_SHADOW = Color.Neumorphic.darkShadow
+
+// character amount to shorten display title to if display title > DT_SHORT
+let DISP_SHORT: Int = 10
+enum Priority: String, CaseIterable
+{
+    case low = "Low"
+    case medium = "Medium"
+    case high = "High"
+}
